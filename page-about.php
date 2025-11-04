0314d6407c0c1706cc9f02057aa4fe50a10ff2b3
Hershey Agustin <hershagustin@gmail.com>
2025-11-03 21:08:57 -0700

    Add files via upload

---
+++ page-about.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: About Page
+*/
+get_header();
+?>
+
+<div class="about-container">  
+    <h2>About</h2> 
+    <div class="about-flex-container">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/about/images/lpp-logo.png" alt="Lollipops Party Packages Logo">
+            <div class="about-inner-container">
+                <p>Lollipop's Party Packages is a family owned business. The name was created with the collaboration of the ideas among the family members, especially the kids.</p>
+    
+                <p>It all started when the family migrated to Canada. Occasionally, we helped friends in decorating their event. Through the encouragement of friends and family, Lollipop's Party Packages was formally launched as a business since May 28 2014.</p>
+    
+                <p>The business aims to cater all kinds of events making it their mission to create memorable arrangements tailor-fitted to the client's needs.</p>
+            </div>        
+    </div>
+</div> 
+
+<?php get_footer(); ?>
